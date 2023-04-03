7c0634968cc8cb5dfc26c1ae59edce000881d260
Enrique Graniel <legraniel.91@gmail.com>
2023-04-03 14:31:12 -0600

    estableciendo las configuraciones necesarias para utilizar el motor de plantillas ejs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const engine = require('ejs-mate'); //plantillas para creación de htmls
+const path = require('path'); //para manejar rutas de archivos, nos permite conectar diversos directorios
 
 const app = express();
 
 //configuraciones
+app.set('views', path.join(__dirname, 'views')); //lo que devuelve esta constante es donde está este archivo, en este caso en la carpeta src
+app.engine('ejs', engine); //aquí le decimos que utilice el motor de plantillas ejs
+app.set('view engine', 'ejs'); //aquí le damos el motor que hemos creado
 app.set('port', process.env.PORT || 3000);
 
 //empezando el servidor
