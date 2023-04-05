b2c9123ee7c320c21ca4c4bc0fd6caf8ead9df78
Enrique Graniel <legraniel.91@gmail.com>
2023-04-05 17:28:17 -0600

    Desarrollando la estructura interna del local autotenfication

--- src/models/user.js
+++ src/models/user.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt-nodejs');
-const { Schema } = mongoose;
+const mongoose = require('mongoose'); //para conectarnos a la base de datos
+const bcrypt = require('bcrypt-nodejs');    //para encriptar las contraseñas
+const { Schema } = mongoose; //para crear esquemas
 
 const userSchema = new Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
-});
+}); //aquí creamos el esquema de la base de datos
 
 userSchema.methods.encryptPassword = (password) => {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-};
+}; //aquí creamos el método para encriptar las contraseñas
 
 userSchema.methods.comparePassword = function (password) {
     return bcrypt.compareSync(password, this.password);
-};
+}; //aquí creamos el método para comparar las contraseñas
 
 module.exports = mongoose.model('users', userSchema);
--- src/passport/local-auth.js
+++ src/passport/local-auth.js
@@ -1,12 +1,18 @@
 const passport = require('passport'); //para autenticar usuarios
 const LocalStrategy = require('passport-local').Strategy;
 
+const User = require('../models/user');
+
 passport.use('local-signup', new LocalStrategy({ //local-signup es el nombre que le damos a la estrategia
     usernameField: 'email',
     passwordField: 'password',
     confirmpasswordField: 'confirm_password',
     passReqToCallback: true
-}, (req, email, password, done) => {
-
+}, asyn(req, email, password, done) => {
+    const user = new User();
+    user.email = email;
+    user.password = password;
+    user.confirm_password = confirm_password;
+    await user.save()
 })); //done es un callback que se ejecuta cuando la autenticación ha sido exitosa
 
