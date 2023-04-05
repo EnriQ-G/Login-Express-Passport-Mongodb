b27d8fa22930e83849aac77faa33070e95655f39
Enrique Graniel <legraniel.91@gmail.com>
2023-04-05 17:17:53 -0600

    creación del metodo que nos ayuda a comprar la contraseña del usuario con la contraseña que tenemos hasheada en nuestra Database

--- src/models/user.js
+++ src/models/user.js
@@ -11,4 +11,8 @@ userSchema.methods.encryptPassword = (password) => {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 };
 
+userSchema.methods.comparePassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
 module.exports = mongoose.model('users', userSchema);
